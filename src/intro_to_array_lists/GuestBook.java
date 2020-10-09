c80c374d6d35ab187610fef6806025f128a535fe
GavinPennock <50836070+GavinPennock@users.noreply.github.com>
2020-10-09 16:58:49 -0700

    did lots
    :100:

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,7 +1,12 @@
 package intro_to_array_lists;
 
+import javax.swing.JFrame;
+
 public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names".
+	JFrame frame=new JFrame();
+	public void run() {
+	frame.setVisible(true);
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
@@ -9,4 +14,5 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	}
 }

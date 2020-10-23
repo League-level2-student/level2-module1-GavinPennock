6867590930900eeb76b4faf227404a48d9f2b894
GavinPennock <50836070+GavinPennock@users.noreply.github.com>
2020-10-23 16:31:55 -0700

    finished
    :100:

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -2,6 +2,7 @@ package intro_to_array_lists;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,26 +15,38 @@ public class GuestBook implements ActionListener {
 	JButton button=new JButton("add name");
 	JPanel panel=new JPanel();
 	JButton button1=new JButton("view names");
-	String[] arrayList=new String[4];
+	ArrayList<String> s = new ArrayList<String>(); 
 	public void run() {
 	frame.setVisible(true);
 	frame.add(panel);
 	panel.add(button);
 	panel.add(button1);
 	button.addActionListener(this);
+	button1.addActionListener(this);
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
+	s.add("Bob Banders");
 	// Guest #2: Sandy Summers
+	s.add("Sandy Summers");
 	// Guest #3: Greg Ganders
+	s.add("Greg Ganders");
 	// Guest #4: Donny Doners
+	s.add("Donny Doners");
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		String names=JOptionPane.showInputDialog(null,"enter the persons name or else");
-		arrayList[4]=names;
+		if(e.getSource()==button) {
+			String name5=JOptionPane.showInputDialog(null,"insert name");
+			s.add(name5);
+		}
+		if(e.getSource()==button1) {
+			for (int i = 0; i < 5; i++) {
+				JOptionPane.showMessageDialog(null, s);
+			}
+		}
+	
 		
 	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -12,20 +12,33 @@ public class IntroToArrayLists {
 		ArrayList<String> s = new ArrayList<String>(); 
 		//2. Add five Strings to your list
 		s.add("a");
-		s.add("s");
+		s.add("se");
 		s.add("d");
-		s.add("f");
+		s.add("fe");
 		s.add("g");
 		//3. Print all the Strings using a standard for-loop
 		for(int i=0; i<5;i++) {
 			System.out.println(s);
 		}
 		//4. Print all the Strings using a for-each loop
-		
+		for(String i: s ) {
+			System.out.println(i);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		for (int i = 0; i < 5; i++) {
+			if(i%2==0) {
+				System.out.println(s.get(i));
+			}
+		}
 		//6. Print all the Strings in reverse order.
-		
+		for (int i = 4; i > -1; i--) {
+		System.out.println(s.get(i));
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		for (int i = 0; i < 5; i++) {
+			if(s.get(i).contains("e")) {
+				System.out.println(s.get(i));
+			}
+		}
 	}	
 }
